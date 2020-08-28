fd506dcb3d8014cb9f45a942af7cff2a79d37e53
yunhoyong <myclub227@gmail.com>
2020-08-28 16:08:33 +0900

    init

---
+++ iloveTennis-service/src/main/java/com/example/demo/IloveTennisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IloveTennisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IloveTennisServiceApplication.class, args);
+	}
+
+}
